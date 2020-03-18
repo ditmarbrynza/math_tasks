ede57db30691f42aca51f6f13b0e6b36fe5afdee
ditmarbrynza <ditmarwork@gmail.com>
2020-03-19 00:18:57 +0300

    refactoring

--- sort/sort.rb
+++ sort/sort.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require 'pry'
 
 class Sort
-
   ONE = '1'
 
   def sort(array)
-    hash = self.hash(array)
-    array = sort_by_units(array, hash)
+    hash = hash(array)
+    sort_by_units(array, hash)
   end
 
   private
@@ -16,18 +17,18 @@ class Sort
     size = arr.length - 1
     while size != 0
       swap = false
-      for i in 0...size
+      (0...size).each do |i|
         x1 = arr[i]
         x2 = arr[i + 1]
         swap = hash[x1] > hash[x2]
-        if hash[x1] > hash[x2]
-          swap = true
-        elsif hash[x1] == hash[x2]
-          swap = arr[i] > arr[i + 1]
-        else
-          swap = false
-        end
-        arr[i], arr[i+1] = arr[i + 1], arr[i] if swap
+        swap = if hash[x1] > hash[x2]
+                 true
+               elsif hash[x1] == hash[x2]
+                 arr[i] > arr[i + 1]
+               else
+                 false
+               end
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] if swap
       end
       size -= 1
     end
@@ -40,7 +41,7 @@ class Sort
       units = calc_units(digit.to_s(2))
       hash[digit] = units
     end
-    hash    
+    hash
   end
 
   def calc_units(str)
@@ -51,11 +52,10 @@ class Sort
     end
     sum
   end
-
 end
 
-output = File.open("./output.txt", 'w+')
-input = File.open("./input.txt", 'r+')
+output = File.open('./output.txt', 'w+')
+input = File.open('./input.txt', 'r+')
 filedata = input.read.split("\n")
 filedata.each do |str|
   sort = Sort.new
@@ -63,6 +63,6 @@ filedata.each do |str|
   arr = arr.each_with_index { |sym, idx| arr[idx] = sym.to_i }
   result = sort.sort(arr)
   output.write("#{result}\n")
-end 
+end
 output.close
 input.close
